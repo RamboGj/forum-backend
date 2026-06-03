d008c3841cffb621a2ce5ea4ab219a83e0261299
artemis <jpramboguanabara@hotmail.com>
2026-06-03 09:10:56 -0300

    feat: comment entities

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,22 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
+import { Comment, CommentProps } from "./comment"
+
+export interface QuestionCommentProps extends CommentProps {
+    answerId: UniqueEntityID
+}
+
+export class QuestionComment extends Comment<QuestionCommentProps> {
+    get answerId() {
+        return this.props.answerId
+    }
+
+    static create(props: Optional<QuestionCommentProps, 'createdAt'>, id?: UniqueEntityID) {
+        const questionComment = new QuestionComment({
+            ...props,
+            createdAt: props.createdAt ?? new Date()
+        }, id)
+    
+        return questionComment
+    }
+}
